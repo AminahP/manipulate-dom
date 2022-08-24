a1f9e8826d0855dfc42a3469fdb975f636062e4c
Aminahp <aypitt7@gmail.com>
2022-08-23 23:48:04 -0400

    complete

--- script.js
+++ script.js
@@ -17,35 +17,67 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
+ document.getElementById("main-title").textContent =  "Dom T's Homepage";
 
 
   // Part 2
-
+document.querySelector('body').style.backgroundColor = 'pink'
 
   // Part 3
-
+const rmLstltm = document.querySelector("li:last-child")
+rmLstltm.parentElemenet.removeChild(rmLstltm);
 
   // Part 4
-
+  const specialTitle = document.querySelectorAll('.special-title');
+  specialTitle.forEach(special => {
+    special.style.fontSize = '2rem';
+  });
 
   // Part 5
-
+  const raceChanges = document.getElementById('past-races');
+  const races = raceChanges.querySelectorAll('li');
+  for (let i = 0; i < races.length; i++) {
+  if (races[i].textContent === "Chicago") {
+    raceChanges.removeChild(races[i]);
+  }};
 
   // Part 6
+  const newRace = document.createElement('li');
+newRace.innerText = 'Punta Cana';
+raceChanges.appendChild(newRace);
 
 
   // Part 7
 
+  const newBlogPost = document.createElement('div');
+document.querySelector('.main').appendChild(newBlogPost);
+
+newBlogPost.classList.add('blog-post');
+newBlogPost.classList.add('purple');
+
+const barcelona = document.createElement('h1');
+barcelona.innerText = "PUNTA CANA";
+newBlogPost.appendChild(puntaCana);
+
+
 
   // Part 8
-
-
+//const randomQuote = function() {
+//   document.querySelector('#quote-of-the-day').innerText = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
+// };
+  const quoteTitle = document.querySelector('#quote-title')
+  quoteTitle.addEventListener('click', randomQuote)
+  
   // Part 9
 
-
-
-
+  const toggler = document.getElementsByClassName('blog-post');
+  Array.from(toggler).forEach(post => {
+    post.addEventListener('mouseout', (event) => {
+      post.classList.toggle('purple');
+    })
+    post.addEventListener('mouseenter', (event) => {
+      post.classList.toggle('red');
+    });
+  });
 });
-////////
-/////
-///
+  // 
